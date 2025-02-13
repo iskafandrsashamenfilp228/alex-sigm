73860c02dcccc9a8fad6b56def040a89348fbae0
Toretocode <aleksandr.filippov0209@gmail.com>
2025-02-13 18:13:26 +0300

    uu
    uu

--- main.js
+++ main.js
@@ -1,23 +1,41 @@
-let dimooooon= (90 + 90 +90) / 3
-console.log(dimooooon,'Димка');
+const dimooooon1 = prompt('Результат Димы в первом раунде?')
+const dimooooon2 = prompt('Результат Димы в втором раунде?')
+const dimooooon3 = prompt('Результат Димы в третьем раунде?')
+const svetik1 = prompt('Результат Светы в первом раунде?')
+const svetik2 = prompt('Результат Светы в втором раунде?')
+const svetik3 = prompt('Результат Светы в третьем раунде?')
+const maksimka1 = prompt('Результат Максима в первом раунде?')
+const maksimka2 = prompt('Результат Максима в втором раунде?')
+const maksimka3 = prompt('Результат Максима в третьем раунде?')
+let dimooooonx = (dimooooon1+ dimooooon2 +dimooooon3)
+console.log(dimooooonx,'Димка');
 
-let maksimka= (90 + 90 +90) / 3
-console.log(maksimka,'Максим');
+let maksimkax= (maksimka1 + maksimka2 + maksimka3)
+console.log(maksimkax,'Максим');
 
-let svetik= (90 + 90 +90) / 3
-console.log(svetik,'Девочка Света');
+let svetikx= (svetik1 + svetik2 + svetik3)
+console.log(svetikx,'Девочка Света');
 
 switch(true){
-  case svetik > dimooooon && svetik > maksimka:
-    console.log('Светлана выйграла!')
+  case svetikx > dimooooonx && svetikx > maksimkax:
+    alert('Светлана выйграла!')
     break;
-  case dimooooon > maksimka && dimooooon > svetik:
-    console.log('Димка выйграл!')
+  case dimooooonx > maksimkax && dimooooonx > svetikx:
+    alert('Димка выйграл!')
     break;
-  case maksimka > dimooooon && maksimka > svetik:
-    console.log('Максим выйграл!')
+  case maksimkax > svetikx && maksimkax > dimooooonx:
+    alert('Максим выйграл!')
+    break;
+  case dimooooonx == maksimkax && dimooooonx > svetikx && maksimkax > svetikx:
+    alert('Свете надо тренероваться!')
+    break;
+  case svetikx > maksimkax && dimooooonx == svetikx && dimooooonx > maksimkax:
+    alert('Максиму надо тренероваться!')
+    break;
+  case dimooooonx < maksimkax && dimooooonx < svetikx && maksimkax == svetikx:
+    alert('Диме надо тренероваться!')
     break;
   default:
-    console.log('Ничья надо тренероваться.');
+    alert('Ничья надо тренероваться.');
     break;  
 }

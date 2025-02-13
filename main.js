815975f9102ee1a1303154a28dee86e4b46343f4
Toretocode <aleksandr.filippov0209@gmail.com>
2025-02-13 17:33:47 +0300

    gg
    ddd

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
-let voin = prompt("Как твоё имя странник?")
-alert("Ступай и победи дракона!" + voin)
+let dimooooon= (90 + 90 +90) / 3
+console.log(dimooooon,'Димка');
+
+let maksimka= (90 + 90 +90) / 3
+console.log(maksimka,'Максим');
+
+let svetik= (90 + 90 +90) / 3
+console.log(svetik,'Девочка Света');
+
+switch(true){
+  case svetik > dimooooon && svetik > maksimka:
+    console.log('Светлана выйграла!')
+    break;
+  case dimooooon > maksimka && dimooooon > svetik:
+    console.log('Димка выйграл!')
+    break;
+  case maksimka > dimooooon && maksimka > svetik:
+    console.log('Максим выйграл!')
+    break;
+  default:
+    console.log('Ничья надо тренероваться.');
+    break;  
+}
